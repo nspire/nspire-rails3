45f3260015a64ad53e99bc8fab9e5d587eac59df
Patrick Kang <jykang1@gmail.com>
2013-10-21 19:18:46 -0400

    rails application initialization

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nspire::Application.config.secret_token = 'a09198f73e93598ee7077f5a96e9605ffa0488e2795136f17f4d8202c7d8f58dd207ea701c826f662796f3d805ce2e16bba0e3cc01fee75de1115f0fd468b3ef'
